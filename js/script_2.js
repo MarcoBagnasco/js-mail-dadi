0ca570dd99e1595feef2764c060ba6f899472354
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-22 14:48:53 +0100

    add dice task 1

--- js/script_2.js
+++ js/script_2.js
@@ -1,4 +1,4 @@
-console.log('hello'); //test
+// console.log('hello'); //test
 
 /* DICE GAME
     1. Generare un numero da 1 a 6 per il giocatore
@@ -6,3 +6,9 @@ console.log('hello'); //test
     3. Confrontare i numeri e stabilire il vincitore in base al numero più grande
     4. Mostrare il vincitore
  */
+
+// 1.
+var userNumber =  Math.floor(Math.random() * 6) + 1;
+console.log(userNumber);  //test
+var computerNumber =  Math.floor(Math.random() * 6) + 1;
+console.log(computerNumber); //test
