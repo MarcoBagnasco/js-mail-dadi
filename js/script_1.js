bf6f860aba1da59ae6467834535f4af99c5125f6
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-22 13:49:56 +0100

    add email task 1

--- js/script_1.js
+++ js/script_1.js
@@ -1,1 +1,11 @@
-console.log('Hello');//test
+// console.log('Hello');//test
+
+/* EMAIL VALIDATION
+    1. Creare una lista di email
+    2. Chiedere all'utente di inserire la propria email
+    3. Verificare se essa è presente nella lista e stampare messagio appropriato
+*/
+
+// 1.
+var emailList = ['marcobagnasco@virgilio.it', 'paoloduzioni@booleanteacher.com', 'lorenzobalsano@booleantutor.com', 'fabiogiorgini@booleantutor.com'];
+console.table(emailList); //test
